37f22b1dd8698e198251571bcf6334fb722c3381
marceaubilger <marceau.bilger@orange.fr>
2023-11-17 08:25:23 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,57 @@
+import os
+directory = "C:\ProjPython"
+
+def list_of_files(directory, extension): 
+    files_names = [] 
+    for filename in os.listdir(directory): 
+        if filename.endswith(extension): 
+            files_names.append(filename) 
+    return files_names
+
+def get_LastNames(files_names):
+    names_files=open("names_files.txt","w+")
+    for i in range(len(files_names)):
+        names_files.write(files_names[i])
+        names_files.write("\n")
+
+    first_replace="Nomination_"
+    sec_replace=".txt"
+    liste_lastNames=[]
+    names_files.seek(0)
+    lines=names_files.readlines()
+    for j in range(len(files_names)):
+        content=lines[j]
+        new_content=content.replace(first_replace,"").replace(sec_replace,"").replace("1","").replace("2","").replace("\n","")
+        liste_lastNames.append(new_content)
+    return liste_lastNames
+
+
+def add_FirstName(liste_lastNames):
+    dic_last_names=list(dict.fromkeys(liste_lastNames))
+    firstNames=open("firstNames.txt","w")
+    for i in range (len(dic_last_names)):
+        if dic_last_names[i]=="Chirac":
+            dic_last_names[i]="Jacques"+" "+dic_last_names[i]+"  "
+        elif dic_last_names[i]=="Giscard dEstaing":
+            dic_last_names[i]="Valerie"+" "+dic_last_names[i]+"  "
+        elif dic_last_names[i]=="Hollande":
+            dic_last_names[i]="Francois"+" "+dic_last_names[i]+"  "
+        elif dic_last_names[i]=="Macron":
+            dic_last_names[i]="Emannuel"+" "+dic_last_names[i]+"  "
+        elif dic_last_names[i]=="Mitterand":
+            dic_last_names[i]="Francois"+" "+dic_last_names[i]+"  "
+        elif dic_last_names[i]=="Sarkozy":
+            dic_last_names[i]="Nicolas"+" "+dic_last_names[i]+"  "
+    for i in range(len(dic_last_names)):
+        firstNames.write(dic_last_names[i])
+        firstNames.write("\n")
+    return dic_last_names
+
+def display_Names(dic_last_names):
+    firstNames=open("firstnames.txt","r")
+    names=firstNames.readlines()
+    for i in range(len(dic_last_names)):
+        print(names[i])
+
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+
+from functions import list_of_files
+from functions import get_LastNames
+from functions import add_FirstName
+from functions import display_Names
+
+directory = "C:\ProjPython\speeches"
+
+files_names = list_of_files(directory, "txt")
+
+liste_lastNames=get_LastNames(files_names)
+
+dic_last_names=add_FirstName(liste_lastNames)
+
+display_Names(dic_last_names)
